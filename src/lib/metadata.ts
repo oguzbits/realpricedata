d19e3241861b0ad56db1998af8570444f878c9ac
oguz <oguz.oeztuerk@outlook.de>
2025-12-28 01:13:09 +0100

    feat: Add multiple favicon sizes and update webmanifest and metadata configuration.

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -26,11 +26,15 @@ export const siteMetadata: Metadata = {
   manifest: "/site.webmanifest",
   icons: {
     icon: [
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon-144x144.png", sizes: "144x144", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
+    shortcut: "/favicon-48x48.png",
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
